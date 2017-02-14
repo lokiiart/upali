86ec5518a520470a7558a81a441a7c759ad5c308
lokiiart <lokiiart@gmail.com>
2017-02-14 12:51:36 +0800

    config mina ended

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,24 +60,35 @@ end
 
 desc "Deploys the current version to the server."
 task :deploy do
-  # uncomment this line to make sure you pushed your local branch to the remote origin
-  # invoke :'git:ensure_pushed'
+  run :local do
+    command %{git add .}
+    command %{git commit -m "`date`"}
+    command %{git push -u origin master}
+  end
+  invoke :'git:ensure_pushed'
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
-    invoke :'deploy:link_shared_paths'
-    invoke :'bundle:install'
-    invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
-    invoke :'deploy:cleanup'
+    command %{docker-compose stop}
+    command %{docker-compose up --build -d}
+  end
+  # # uncomment this line to make sure you pushed your local branch to the remote origin
+  # # invoke :'git:ensure_pushed'
+  # deploy do
+  #   # Put things that will set up an empty directory into a fully set-up
+  #   # instance of your project.
+  #   invoke :'git:clone'
+  #   invoke :'deploy:link_shared_paths'
+  #   invoke :'bundle:install'
+  #   invoke :'rails:db_migrate'
+  #   invoke :'rails:assets_precompile'
+  #   invoke :'deploy:cleanup'
 
-    on :launch do
-      in_path(fetch(:current_path)) do
-        command %{mkdir -p tmp/}
-        command %{touch tmp/restart.txt}
-      end
-    end
+  #   on :launch do
+  #     in_path(fetch(:current_path)) do
+  #       command %{mkdir -p tmp/}
+  #       command %{touch tmp/restart.txt}
+  #     end
+  #   end
   end
 
   # you can use `run :local` to run tasks on local machine before of after the deploy scripts
